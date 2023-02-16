7e990f950375a370b3271ad2b662a4d1c53bcf6f
Zou Hanya <hanyazou@gmail.com>
2023-02-16 19:53:56 +0900

    Roughly implemented CP/M disk I/O

--- emuz80_z80ram.c
+++ emuz80_z80ram.c
@@ -83,6 +83,10 @@
 #include <string.h>
 #include <ff.h>
 #include <SDCard.h>
+#include <utils.h>
+
+#define CPM_DISK_DEBUG
+#define CPM_MEM_DEBUG
 
 #define Z80_CLK 6000000UL       // Z80 clock frequency(Max 16MHz)
 
@@ -100,12 +104,41 @@
 #define _XTAL_FREQ 64000000UL
 #define SPI_CLOCK_100KHZ 10     // Determined by actual measurement
 #define SPI_CLOCK_2MHZ 0        // Maximum speed w/o any wait (1~2 MHz)
+#define NUM_FILES 8
+#define SECTOR_SIZE 128
 
 // Z80 ROM equivalent, see end of this file
 extern const unsigned char rom[];
 static FATFS fs;
 static FIL file;
-static uint8_t buf[128];
+static uint8_t buf[SECTOR_SIZE];
+
+typedef struct {
+    unsigned int sectors;
+    FIL *filep;
+} drive_t;
+drive_t drives[] = {
+    { 26 },
+    { 26 },
+    { 26 },
+    { 26 },
+    { 0 },
+    { 0 },
+    { 0 },
+    { 0 },
+    { 128 },
+    { 128 },
+    { 128 },
+    { 128 },
+    { 0 },
+    { 0 },
+    { 0 },
+    { 16484 },
+};
+FIL files[NUM_FILES];
+int num_files = 0;
+
+#define NUM_DRIVES (sizeof(drives)/sizeof(*drives))
 
 // Address Bus
 union {
@@ -144,7 +177,7 @@ void __interrupt(irq(CLC3),base(8)) CLC_ISR() {
         switch (ab.l) {
         case UART_DREG:
             while(!U3TXIF);
-            U3TXB = PORTC;          // Write into    U3TXB
+            U3TXB = PORTC;      // Write into    U3TXB
             break;
         case DISK_REG_DRIVE:
             disk_drive = PORTC;
@@ -158,8 +191,10 @@ void __interrupt(irq(CLC3),base(8)) CLC_ISR() {
         case DISK_REG_FDCOP:
             disk_op = PORTC;
             do_disk_io = 1;
+            #ifdef CPM_DISK_DEBUG
             printf("DISK: OP=%02x D/T/S=%d/%d/%d ADDR=%02x%02x ...\n\r", disk_op,
                    disk_drive, disk_track, disk_sector, disk_dmah, disk_dmal, PORTC);
+            #endif
             break;
         case DISK_REG_DMAL:
             disk_dmal = PORTC;
@@ -169,48 +204,125 @@ void __interrupt(irq(CLC3),base(8)) CLC_ISR() {
             break;
         }
         if (!do_disk_io) {
-        // Release wait (D-FF reset)
-        G3POL = 1;
-        G3POL = 0;
-        CLC3IF = 0;             // Clear interrupt flag
-        return;
+            // Release wait (D-FF reset)
+            G3POL = 1;
+            G3POL = 0;
+            CLC3IF = 0;         // Clear interrupt flag
+            return;
         }
 
         //
         // Do disk I/O
         //
-        LATE0 = 0;           // /BUSREQ is active
-        RA4PPS = 0x00;       // unbind CLC1 and RA4 -> /OE
-        G3POL = 1;           // Release wait (D-FF reset)
+        LATE0 = 0;          // /BUSREQ is active
+        RA4PPS = 0x00;      // unbind CLC1 and /OE (RA4)
+        RA2PPS = 0x00;      // unbind CLC2 and /WE (RA2)
+        LATA4 = 1;          // deactivate /OE
+        LATA2 = 1;          // deactivate /WE
+
+        G3POL = 1;          // Release wait (D-FF reset)
         G3POL = 0;
 
-        // XXX
-        ANSELA4 = 0;         // Disable analog function
-        LATA4 = 1;           // /OE is deactive
-        TRISA4 = 0;          // Set as output
-
-        int n;
-        if (f_open(&file, "/HELLOFAT.TXT", FA_READ|FA_WRITE) != FR_OK) {
-            printf("f_open(): ERROR\n\r");
+        if (NUM_DRIVES <= disk_drive || drives[disk_drive].filep == NULL) {
+            disk_stat = 0xff;   // error
+            goto disk_io_done;
         }
-        if (f_read(&file, buf, sizeof(buf), &n) != FR_OK) {
-            printf("f_read(): ERROR\n\r");
-        }
-        buf[n] = '\0';
-        for (char *p = buf; *p; p++) {
-            if (*p == 0x0d || *p == 0x0a)
-                *p = ' ';
-        }
-        f_close(&file);
-        printf("/HELLOFAT.TXT: %d bytes, \"%s\"\n\r", n, buf);
 
-        disk_stat = 0xff;    // XXX, error
+        // Set address bus as output
+        TRISD = 0x00;       // A15-A8 pin (A14:/RFSH, A15:/WAIT)
+        TRISB = 0x00;       // A7-A0
 
+        uint32_t sector = disk_track * drives[disk_drive].sectors + disk_sector - 1;
+        FIL *filep = drives[disk_drive].filep;
+        unsigned int n;
+        if (f_lseek(filep, sector * SECTOR_SIZE) != FR_OK) {
+            printf("f_lseek(): ERROR\n\r");
+            disk_stat = 0xff;   // error
+            goto disk_io_done;
+        }
+        if (disk_op == 0) {
+            //
+            // DISK read
+            //
+
+            // read from the DISK
+            if (f_read(filep, buf, SECTOR_SIZE, &n) != FR_OK) {
+                printf("f_read(): ERROR\n\r");
+                disk_stat = 0xff;  // error
+                goto disk_io_done;
+            }
+
+            // transfer read data to SRAM
+            uint16_t addr = ((uint16_t)disk_dmah << 8) | disk_dmal;
+            TRISC = 0x00;       // Set as output to write to the SRAM
+            for(int i = 0; i < SECTOR_SIZE; i++) {
+                ab.w = addr;
+                LATD = ab.h;
+                LATB = ab.l;
+                addr++;
+                LATA2 = 0;      // activate /WE
+                LATC = buf[i];
+                LATA2 = 1;      // deactivate /WE
+            }
+
+            #ifdef CPM_MEM_DEBUG
+            // read back the SRAM
+            uint16_t addr = ((uint16_t)disk_dmah << 8) | disk_dmal;
+            printf("f_read(): SRAM address: %04x\n\r", addr);
+            TRISC = 0xff;       // Set as input to read from the SRAM
+            for(int i = 0; i < SECTOR_SIZE; i++) {
+                ab.w = addr;
+                LATD = ab.h;
+                LATB = ab.l;
+                addr++;
+                LATA4 = 0;      // activate /OE
+                buf[i] = PORTC;
+                LATA4 = 1;      // deactivate /OE
+            }
+            util_hexdump("RAM: ", buf, SECTOR_SIZE);
+            #endif  // CPM_MEM_DEBUG
+        } else {
+            //
+            // DISK write
+            //
+
+            // transfer write data from SRAM to the buffer
+            uint16_t addr = ((uint16_t)disk_dmah << 8) | disk_dmal;
+            TRISC = 0xff;       // Set as input to read from the SRAM
+            for(int i = 0; i < SECTOR_SIZE; i++) {
+                ab.w = addr;
+                LATD = ab.h;
+                LATB = ab.l;
+                addr++;
+                LATA4 = 0;      // activate /OE
+                buf[i] = LATC;
+                LATA4 = 1;      // deactivate /OE
+            }
+
+            // write buffer to the DISK
+            if (f_write(filep, buf, SECTOR_SIZE, &n) != FR_OK || n != strlen(buf)) {
+                printf("f_write(): ERROR\n\r");
+                disk_stat = 0xff;  // error
+                goto disk_io_done;
+            }
+        }
+
+        disk_stat = 0x00;       // disk I/O succeeded
+
+    disk_io_done:
+        #ifdef CPM_DISK_DEBUG
         printf("DISK: OP=%02x D/T/S=%d/%d/%d ADDR=%02x%02x ... ST=%02x\n\r", disk_op,
                disk_drive, disk_track, disk_sector, disk_dmah, disk_dmal, disk_stat);
+        #endif
 
-        LATE0 = 1;           // /BUSREQ is deactive
-        RA4PPS = 0x01;       // CLC1 -> RA4 -> /OE
+        // Set address bus as input
+        TRISD = 0x7f;           // A15-A8 pin (A14:/RFSH, A15:/WAIT)
+        TRISB = 0xff;           // A7-A0 pin
+        TRISC = 0xff;           // D7-D0 pin
+
+        RA4PPS = 0x01;          // CLC1 -> RA4 -> /OE
+        RA2PPS = 0x02;          // CLC2 -> RA2 -> /WE
+        LATE0 = 1;              // /BUSREQ is deactive
 
         CLC3IF = 0;             // Clear interrupt flag
         return;
@@ -236,6 +348,7 @@ void __interrupt(irq(CLC3),base(8)) CLC_ISR() {
     // Release wait (D-FF reset)
     G3POL = 1;
     G3POL = 0;
+
     // Post processing
 #if 1
     while(!RA0);                // /IORQ <5.6MHz
@@ -326,43 +439,31 @@ void main(void) {
 
     RA2PPS = 0x00;      // LATA2 -> RA2
 
-    {
-        unsigned int n;
-
-        SDCard_init(SPI_CLOCK_100KHZ, SPI_CLOCK_2MHZ, /* timeout */ 100);
-        if (f_mount(&fs, "0://", 1) != FR_OK) {
-            printf("f_mount(): ERROR\n\r");
+    //
+    // Initialize SD Card
+    //
+    SDCard_init(SPI_CLOCK_100KHZ, SPI_CLOCK_2MHZ, /* timeout */ 100);
+    if (f_mount(&fs, "0://", 1) != FR_OK) {
+        printf("f_mount(): ERROR\n\r");
+    } else {
+        //
+        // Open disk images
+        //
+        for (unsigned int drive = 0; drive < NUM_DRIVES && num_files < NUM_FILES; drive++) {
+            char drive_letter = 'A' + drive;
+            sprintf(buf, "CPMDISKS/DRIVE%c.DSK", drive_letter);
+            if (f_open(&files[num_files], buf, FA_READ|FA_WRITE) == FR_OK) {
+                printf("Image file DRIVE%c.DSK is assigned to drive %c\n\r",
+                       drive_letter, drive_letter);
+                drives[drive].filep = &files[num_files];
+                num_files++;
+            }
         }
-        if (f_open(&file, "/HELLOFAT.TXT", FA_READ|FA_WRITE) != FR_OK) {
-            printf("f_open(): ERROR\n\r");
-        }
-        if (f_read(&file, buf, sizeof(buf), &n) != FR_OK) {
-            printf("f_read(): ERROR\n\r");
-        }
-        buf[n] = '\0';
-        for (char *p = buf; *p; p++) {
-            if (*p == 0x0d || *p == 0x0a)
-                *p = ' ';
-        }
-        printf("/HELLOFAT.TXT: %d bytes, \"%s\"\n\r", n, buf);
-        if (f_lseek(&file, 0) != FR_OK) {
-            printf("f_lseek(): ERROR\n\r");
-        }
-        if (buf[0] == 'H') {
-            sprintf(buf, "Good bye, FAT world!\n");
-        } else {
-            sprintf(buf, "Hello, FAT world!\n");
-        }
-        if (f_truncate(&file) != FR_OK) {
-            printf("f_truncate(): ERROR\n\r");
-        }
-        printf("f_write(): %s\n\r", buf);
-        if (f_write (&file, buf, strlen(buf), &n) != FR_OK || n != strlen(buf)) {
-            printf("f_write(): ERROR\n\r");
-        }
-        f_close(&file);
     }
 
+    //
+    // Transfer ROM image to the SRAM
+    //
     for(i = 0; i < ROM_SIZE; i++) {
         ab.w = i;
         LATD = ab.h;
