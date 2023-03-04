57f339c093eb38d27cec8bedd55d9c13b1b15a14
Zou Hanya <hanyazou@gmail.com>
2023-03-04 19:07:23 +0900

    Fix SPI.c to handle multiple SPI interfaces

--- diskio/SPI.c
+++ diskio/SPI.c
@@ -25,62 +25,66 @@
 #include <stdio.h>
 #include "SPI.h"
 
-#define SPI_PICO LATC0  // controller data out
-#define SPI_CLK	 LATC1  // clock
-#define SPI_POCI ((PORTC & 0x04) ? 1 : 0)  // controller data in
-#define SPI_CS   LATE2  // chip select
+#define __C(a, b) a ## _ ## b
+#define C(a, b) __C(a, b)
+#define SPI(a) C(SPI_PREFIX, a)
 
-static struct SPI {
+static struct SPI_SW {
+    struct SPI spi;
     uint8_t clk_phase;
     uint8_t clk_polarity;
     uint8_t bit_order;
     uint8_t trisc;
     uint8_t clock_delay;
-    void (*send)(void *buf, int count);
-    void (*receive)(void *buf, int count);
-} ctx_ = { 0 };
-#define ctx (&ctx_)
+    void (*send)(struct SPI *ctx_, void *buf, int count);
+    void (*receive)(struct SPI *ctx_, void *buf, int count);
+};
 
-void SPI_begin(void)
+static void SPI(send_mode0_msbfirst_nowait)(struct SPI *ctx_, void *buf, int count);
+static void SPI(receive_mode0_msbfirst_nowait)(struct SPI *ctx_, void *buf, int count);
+
+void SPI(begin)(struct SPI *ctx_)
 {
-    ANSELE2 = 0;  // Disable analog function
-    SPI_CS = 1;  // Inactive
-    TRISE2 = 0;  // Set as output
+    SPI(CS_ANSEL) = 0;  // Disable analog function
+    SPI(CS) = 1;  // Inactive
+    SPI(CS_TRIS) = 0;  // Set as output
 }
 
-void SPI_configure(uint16_t clock_delay, uint8_t bit_order, uint8_t data_mode)
+void SPI(configure)(struct SPI *ctxx2, uint16_t clock_delay, uint8_t bit_order, uint8_t data_mode)
 {
+    struct SPI_SW *ctx = (struct SPI_SW *)ctxx2;
     ctx->clk_phase = (data_mode & 1);  // clock phase (CPHA)
     ctx->clk_polarity = (data_mode & 2);  // clock polarity (CPOL)
     ctx->bit_order = bit_order;
     ctx->clock_delay = clock_delay;
 
     if (data_mode == SPI_MODE0 && bit_order == SPI_MSBFIRST && clock_delay == 0) {
-        ctx->send = SPI_send_mode0_msbfirst_nowait;
-        ctx->receive = SPI_receive_mode0_msbfirst_nowait;
+        ctx->send = SPI(send_mode0_msbfirst_nowait);
+        ctx->receive = SPI(receive_mode0_msbfirst_nowait);
     } else {
         ctx->send = NULL;
         ctx->receive = NULL;
     }
 }
 
-void SPI_begin_transaction(void)
+void SPI(begin_transaction)(struct SPI *ctx_)
 {
+    struct SPI_SW *ctx = (struct SPI_SW *)ctx_;
     ctx->trisc = TRISC;  // save direction settings
-    TRISC0 = 0;  // set PICO as output
-    TRISC1 = 0;  // set clock as output
-    TRISC2 = 1;  // set POCI as input
+    SPI(PICO_TRIS) = 0;  // set PICO as output
+    SPI(CLK_TRIS) = 0;   // set clock as output
+    SPI(POCI_TRIS) = 1;  // set POCI as input
 
     //TRISC = TRISC & 0xfc;  // XXX
     //TRISC = TRISC | 0x04;  // XXX
 
-    SPI_CLK = ctx->clk_polarity ? 1 : 0;  // set clock in idle state
-    SPI_PICO = 1;  // set PICO as high (SPI standard does not require this but SD card does)
-    SPI_CS = 0;  // start transaction
+    SPI(CLK) = ctx->clk_polarity ? 1 : 0;  // set clock in idle state
+    SPI(PICO) = 1;  // set PICO as high (SPI standard does not require this but SD card does)
+    SPI(CS) = 0;  // start transaction
 }
 
 // reverse bit order of given byte
-uint8_t SPI_reverse_bit_order(uint8_t val)
+static uint8_t SPI_reverse_bit_order(uint8_t val)
 {
     static uint8_t high[] = {
         0x00, 0x08, 0x04, 0x0c, 0x02, 0x0a, 0x06, 0x0e,
@@ -95,8 +99,9 @@ uint8_t SPI_reverse_bit_order(uint8_t val)
         for (volatile uint_fast8_t i = ctx->clock_delay; i != 0; i--) ;  \
     } while(0)
 
-uint8_t SPI_transfer_byte(uint8_t output)
+uint8_t SPI(transfer_byte)(struct SPI *ctx_, uint8_t output)
 {
+    struct SPI_SW *ctx = (struct SPI_SW *)ctx_;
     uint8_t input = 0;
     if (ctx->bit_order == SPI_MSBFIRST) {
         output = SPI_reverse_bit_order(output);
@@ -106,24 +111,24 @@ uint8_t SPI_transfer_byte(uint8_t output)
 
     for (int i = 0; i < 8; i++) {
         // write output data
-        SPI_PICO = (output & 1);
+        SPI(PICO) = (output & 1);
         output >>= 1;
 
         if (ctx->clk_phase) {
             // read input data at second edge if clock phase (CPHA) is 1
-            SPI_CLK = (clk ^= 1);
+            SPI(CLK) = (clk ^= 1);
             SPI_clock_delay();
         }
 
-        SPI_CLK = (clk ^= 1);
+        SPI(CLK) = (clk ^= 1);
 
         // read intput data
-        input = (input << 1) | SPI_POCI;
+        input = (input << 1) | SPI(POCI);
         SPI_clock_delay();
 
         if (!ctx->clk_phase) {
             // read input data at first edge if clock phase (CPHA) is 0
-            SPI_CLK = (clk ^= 1);
+            SPI(CLK) = (clk ^= 1);
             SPI_clock_delay();
         }
     }
@@ -135,16 +140,16 @@ uint8_t SPI_transfer_byte(uint8_t output)
     return input;
 }
 
-void SPI_transfer(void *buf, int count)
+void SPI(transfer)(struct SPI *ctx_, void *buf, int count)
 {
     uint8_t *p = (uint8_t*)buf;
     for (int i = 0; i < count; i++) {
-        *p = SPI_transfer_byte(*p);
+        *p = SPI(transfer_byte)(ctx_, *p);
         p++;
     }
 }
 
-void SPI_send_mode0_msbfirst_nowait(void *buf, int count)
+static void SPI(send_mode0_msbfirst_nowait)(struct SPI *ctx_, void *buf, int count)
 {
     uint8_t *p = (uint8_t*)buf;
     uint8_t *endp = p + count;
@@ -153,171 +158,191 @@ void SPI_send_mode0_msbfirst_nowait(void *buf, int count)
         uint8_t output = *p++;
 
         // write output data bit 7
-        SPI_PICO = (output & 0x80) ? 1 : 0;
-        SPI_CLK = 1;
+        SPI(PICO) = (output & 0x80) ? 1 : 0;
+        SPI(CLK) = 1;
         asm("nop");
-        SPI_CLK = 0;
+        SPI(CLK) = 0;
 
         // write output data bit 6
-        SPI_PICO = (output & 0x40) ? 1 : 0;
-        SPI_CLK = 1;
+        SPI(PICO) = (output & 0x40) ? 1 : 0;
+        SPI(CLK) = 1;
         asm("nop");
-        SPI_CLK = 0;
+        SPI(CLK) = 0;
 
         // write output data bit 5
-        SPI_PICO = (output & 0x20) ? 1 : 0;
-        SPI_CLK = 1;
+        SPI(PICO) = (output & 0x20) ? 1 : 0;
+        SPI(CLK) = 1;
         asm("nop");
-        SPI_CLK = 0;
+        SPI(CLK) = 0;
 
         // write output data bit 4
-        SPI_PICO = (output & 0x10) ? 1 : 0;
-        SPI_CLK = 1;
+        SPI(PICO) = (output & 0x10) ? 1 : 0;
+        SPI(CLK) = 1;
         asm("nop");
-        SPI_CLK = 0;
+        SPI(CLK) = 0;
 
         // write output data bit 3
-        SPI_PICO = (output & 0x08) ? 1 : 0;
-        SPI_CLK = 1;
+        SPI(PICO) = (output & 0x08) ? 1 : 0;
+        SPI(CLK) = 1;
         asm("nop");
-        SPI_CLK = 0;
+        SPI(CLK) = 0;
 
         // write output data bit 2
-        SPI_PICO = (output & 0x04) ? 1 : 0;
-        SPI_CLK = 1;
+        SPI(PICO) = (output & 0x04) ? 1 : 0;
+        SPI(CLK) = 1;
         asm("nop");
-        SPI_CLK = 0;
+        SPI(CLK) = 0;
 
         // write output data bit 1
-        SPI_PICO = (output & 0x02) ? 1 : 0;
-        SPI_CLK = 1;
+        SPI(PICO) = (output & 0x02) ? 1 : 0;
+        SPI(CLK) = 1;
         asm("nop");
-        SPI_CLK = 0;
+        SPI(CLK) = 0;
 
         // write output data bit 0
-        SPI_PICO = (output & 0x01) ? 1 : 0;
-        SPI_CLK = 1;
+        SPI(PICO) = (output & 0x01) ? 1 : 0;
+        SPI(CLK) = 1;
         asm("nop");
-        SPI_CLK = 0;
+        SPI(CLK) = 0;
     }
 }
 
-void SPI_receive_mode0_msbfirst_nowait(void *buf, int count)
+static void SPI(receive_mode0_msbfirst_nowait)(struct SPI *ctx_, void *buf, int count)
 {
     uint8_t *p = (uint8_t*)buf;
     uint8_t *endp = p + count;
 
-    SPI_PICO = 1;
+    SPI(PICO) = 1;
 
     while (p < endp) {
         uint8_t input = 0;
 
         // read input data bit 7
-        SPI_CLK = 1;
-        input |= SPI_POCI;
-        SPI_CLK = 0;
+        SPI(CLK) = 1;
+        input |= SPI(POCI);
+        SPI(CLK) = 0;
 
         // read input data bit 6
         input <<= 1;
-        SPI_CLK = 1;
-        input |= SPI_POCI;
-        SPI_CLK = 0;
+        SPI(CLK) = 1;
+        input |= SPI(POCI);
+        SPI(CLK) = 0;
 
         // read input data bit 5
         input <<= 1;
-        SPI_CLK = 1;
-        input |= SPI_POCI;
-        SPI_CLK = 0;
+        SPI(CLK) = 1;
+        input |= SPI(POCI);
+        SPI(CLK) = 0;
 
         // read input data bit 4
         input <<= 1;
-        SPI_CLK = 1;
-        input |= SPI_POCI;
-        SPI_CLK = 0;
+        SPI(CLK) = 1;
+        input |= SPI(POCI);
+        SPI(CLK) = 0;
 
         // read input data bit 3
         input <<= 1;
-        SPI_CLK = 1;
-        input |= SPI_POCI;
-        SPI_CLK = 0;
+        SPI(CLK) = 1;
+        input |= SPI(POCI);
+        SPI(CLK) = 0;
 
         // read input data bit 2
         input <<= 1;
-        SPI_CLK = 1;
-        input |= SPI_POCI;
-        SPI_CLK = 0;
+        SPI(CLK) = 1;
+        input |= SPI(POCI);
+        SPI(CLK) = 0;
 
         // read input data bit 1
         input <<= 1;
-        SPI_CLK = 1;
-        input |= SPI_POCI;
-        SPI_CLK = 0;
+        SPI(CLK) = 1;
+        input |= SPI(POCI);
+        SPI(CLK) = 0;
 
         // read input data bit 0
         input <<= 1;
-        SPI_CLK = 1;
-        input |= SPI_POCI;
-        SPI_CLK = 0;
+        SPI(CLK) = 1;
+        input |= SPI(POCI);
+        SPI(CLK) = 0;
 
         *p++ = input;
     }
 }
 
-void SPI_send(void *buf, int count)
+void SPI(send)(struct SPI *ctx_, void *buf, int count)
 {
+    struct SPI_SW *ctx = (struct SPI_SW *)ctx_;
     if (ctx->send) {
-        (*ctx->send)(buf, count);
+        (*ctx->send)(ctx_, buf, count);
     } else {
         uint8_t *p = (uint8_t*)buf;
         for (int i = 0; i < count; i++) {
-            SPI_transfer_byte(*p);
+            SPI(transfer_byte)(ctx_, *p);
             p++;
         }
     }
 }
 
-void SPI_receive(void *buf, int count)
+void SPI(receive)(struct SPI *ctx_, void *buf, int count)
 {
+    struct SPI_SW *ctx = (struct SPI_SW *)ctx_;
     if (ctx->receive) {
-        (*ctx->receive)(buf, count);
+        (*ctx->receive)(ctx_, buf, count);
     } else {
         uint8_t *p = (uint8_t*)buf;
         for (int i = 0; i < count; i++) {
-            *p++ = SPI_transfer_byte(0xff);
+            *p++ = SPI(transfer_byte)(ctx_, 0xff);
         }
     }
 }
 
-void SPI_end_transaction(void)
+void SPI(end_transaction)(struct SPI *ctx_)
 {
-    SPI_CS = 1;  // end transaction
+    struct SPI_SW *ctx = (struct SPI_SW *)ctx_;
+    SPI(CS) = 1;  // end transaction
     TRISC = ctx->trisc;  // restore direction settings
 }
 
-void SPI_dummy_clocks(int clocks)
+void SPI(dummy_clocks)(struct SPI *ctx_, int clocks)
 {
+    struct SPI_SW *ctx = (struct SPI_SW *)ctx_;
     if (ctx->clock_delay == 0) {
-        SPI_PICO = 1;
+        SPI(PICO) = 1;
         clocks *= 8;
 
         while (clocks--) {
-            SPI_CLK = 1;
+            SPI(CLK) = 1;
             asm("nop");
-            SPI_CLK = 0;
+            SPI(CLK) = 0;
         }
         return;
     }
 
     uint8_t dummy = 0xff;
     for (int i = 0; i < clocks; i++) {
-        SPI_send(&dummy, 1);
+        SPI(send)(ctx_, &dummy, 1);
     }
 }
 
-uint8_t SPI_receive_byte(void)
+uint8_t SPI(receive_byte)(struct SPI *ctx_)
 {
     uint8_t dummy = 0xff;
-    SPI_receive(&dummy, 1);
+    SPI(receive)(ctx_, &dummy, 1);
     return dummy;
 }
 
+static struct SPI_SW ctx_ = {
+    {
+        SPI(begin),
+        SPI(configure),
+        SPI(begin_transaction),
+        SPI(transfer_byte),
+        SPI(transfer),
+        SPI(send),
+        SPI(receive),
+        SPI(end_transaction),
+        SPI(dummy_clocks),
+        SPI(receive_byte),
+    },
+};
+
+struct SPI *SPI(ctx) = (struct SPI *)&ctx_;
--- diskio/SPI.h
+++ diskio/SPI.h
@@ -24,6 +24,26 @@
 #ifndef __SPI_H__
 #define __SPI_H__
 
+#define SPI0_PICO      LATC0  // controller data out
+#define SPI0_PICO_TRIS TRISC0
+#define SPI0_CLK       LATC1  // clock
+#define SPI0_CLK_TRIS  TRISC1
+#define SPI0_POCI      ((PORTC & 0x04) ? 1 : 0)  // controller data in
+#define SPI0_POCI_TRIS TRISC2
+#define SPI0_CS        LATE2  // chip select
+#define SPI0_CS_TRIS   TRISE2
+#define SPI0_CS_ANSEL  ANSELE2
+
+#define SPI1_PICO      LATC3  // controller data out
+#define SPI1_PICO_TRIS TRISC3
+#define SPI1_CLK       LATC4  // clock
+#define SPI1_CLK_TRIS  TRISC4
+#define SPI1_POCI      ((PORTC & 0x20) ? 1 : 0)  // controller data in
+#define SPI1_POCI_TRIS TRISC5
+#define SPI1_CS        LATE2  // chip select
+#define SPI1_CS_TRIS   TRISE2
+#define SPI1_CS_ANSEL  ANSELE2
+
 #define SPI_MSBFIRST 1
 #define SPI_LSBFIRST 0
 #define SPI_MODE0 0
@@ -31,20 +51,51 @@
 #define SPI_MODE2 2
 #define SPI_MODE3 3
 
-void SPI_begin(void);
-void SPI_configure(uint16_t clock_delay, uint8_t bitOrder, uint8_t dataMode);
-void SPI_begin_transaction(void);
-uint8_t SPI_transfer_byte(uint8_t output);
-void SPI_transfer(void *buf, int count);
-void SPI_send(void *buf, int count);
-void SPI_receive(void *buf, int count);
-void SPI_end_transaction(void);
+struct SPI {
+    void (*begin)(struct SPI *ctx_);
+    void (*configure)(struct SPI *ctx_, uint16_t clock_delay, uint8_t bitOrder, uint8_t dataMode);
+    void (*begin_transaction)(struct SPI *ctx_);
+    uint8_t (*transfer_byte)(struct SPI *ctx_, uint8_t output);
+    void (*transfer)(struct SPI *ctx_, void *buf, int count);
+    void (*send)(struct SPI *ctx_, void *buf, int count);
+    void (*receive)(struct SPI *ctx_, void *buf, int count);
+    void (*end_transaction)(struct SPI *ctx_);
+    void (*dummy_clocks)(struct SPI *ctx_, int clocks);
+    uint8_t (*receive_byte)(struct SPI *ctx_);
+};
 
-void SPI_send_mode0_msbfirst_nowait(void *buf, int count);
-void SPI_receive_mode0_msbfirst_nowait(void *buf, int count);
+extern struct SPI *SPI0_ctx;
+extern struct SPI *SPI1_ctx;
 
-void SPI_dummy_clocks(int clocks);
-uint8_t SPI_receive_byte(void);
-uint8_t SPI_reserve_bit_order(uint8_t val);
+static inline void SPI_begin(void) {
+    SPI0_ctx->begin(SPI0_ctx);
+}
+static inline void SPI_configure(uint16_t clock_delay, uint8_t bitOrder, uint8_t dataMode) {
+    SPI0_ctx->configure(SPI0_ctx, clock_delay, bitOrder, dataMode);
+}
+static inline void SPI_begin_transaction(void) {
+    SPI0_ctx->begin_transaction(SPI0_ctx);
+}
+static inline uint8_t SPI_transfer_byte(uint8_t output) {
+    return SPI0_ctx->transfer_byte(SPI0_ctx, output);
+}
+static inline void SPI_transfer(void *buf, int count) {
+    SPI0_ctx->transfer(SPI0_ctx, buf, count);
+}
+static inline void SPI_send(void *buf, int count) {
+    SPI0_ctx->send(SPI0_ctx, buf, count);
+}
+static inline void SPI_receive(void *buf, int count) {
+    SPI0_ctx->receive(SPI0_ctx, buf, count);
+}
+static inline void SPI_end_transaction(void) {
+    SPI0_ctx->end_transaction(SPI0_ctx);
+}
+static inline void SPI_dummy_clocks(int clocks) {
+    SPI0_ctx->dummy_clocks(SPI0_ctx, clocks);
+}
+static inline uint8_t SPI_receive_byte(void) {
+    return SPI0_ctx->receive_byte(SPI0_ctx);
+}
 
 #endif  // __SPI_H__
